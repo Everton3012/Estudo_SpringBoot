d881969286ca3fb8197651c6591f71dccb3d4f30
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-09-06 18:54:02 -0300

    add endpoint to retrieve client by ID in ClienteController

--- src/main/java/br/com/Everton3012/exercicios_sb/controllers/ClienteController.java
+++ src/main/java/br/com/Everton3012/exercicios_sb/controllers/ClienteController.java
@@ -2,6 +2,7 @@ package br.com.Everton3012.exercicios_sb.controllers;
 
 import br.com.Everton3012.exercicios_sb.models.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class ClienteController {
     @GetMapping(path="/qualquer")
     public Cliente getCliente(){
         return new Cliente(1, "Everton", "123.456.789-00");
+    }
+
+    @GetMapping(path="/{id}")
+    public  Cliente getClienteId(@PathVariable int id){
+        return new Cliente(id, "Everton", "123.456.789-00");
     }
 }
