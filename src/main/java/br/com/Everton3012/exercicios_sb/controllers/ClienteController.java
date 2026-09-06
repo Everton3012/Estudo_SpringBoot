c682b7c932130a000d1e0ec49d1686c4f4e8badd
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-09-06 19:01:44 -0300

    add endpoint to retrieve client by request parameter in ClienteController

--- src/main/java/br/com/Everton3012/exercicios_sb/controllers/ClienteController.java
+++ src/main/java/br/com/Everton3012/exercicios_sb/controllers/ClienteController.java
@@ -1,10 +1,7 @@
 package br.com.Everton3012.exercicios_sb.controllers;
 
 import br.com.Everton3012.exercicios_sb.models.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/clientes")
@@ -17,5 +14,10 @@ public class ClienteController {
     @GetMapping(path="/{id}")
     public  Cliente getClienteId(@PathVariable int id){
         return new Cliente(id, "Everton", "123.456.789-00");
+    }
+
+    @GetMapping
+    public  Cliente getClienteId2(@RequestParam(name="id", defaultValue = "1") int id){
+        return new Cliente(id, "David", "111.222.333-00");
     }
 }
