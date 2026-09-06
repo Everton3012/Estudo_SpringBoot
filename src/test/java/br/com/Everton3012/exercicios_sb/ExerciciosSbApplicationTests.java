4e9b331f3ab561e52fd009c054bd6cff11e61762
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-09-05 22:13:14 -0300

    initialize Spring Boot application with Maven setup and project structure

---
+++ src/test/java/br/com/Everton3012/exercicios_sb/ExerciciosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Everton3012.exercicios_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
